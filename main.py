67c931291361285c13a623cb167f475bd96d5dc1
Paul Dibble <paul.dibble3@gmail.com>
2020-11-30 12:02:10 -0800

    a

---
+++ main.py
@@ -0,0 +1,6 @@
+def printHelloworld():
+    print('HelloWorld')
+    return 'HelloWorld'
+
+def Sum(num1, num2):
+    return num1+num2
---
+++ test.py
@@ -0,0 +1,7 @@
+import main
+
+def test_sum():
+    assert(main.Sum(1,3) ==4)
+
+test_sum()
+    
